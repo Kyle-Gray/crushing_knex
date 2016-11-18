804825d72133aa9070e646f402fdc2209a6c2dde
Kyle Gray <th33nd3r@gmail.com>
2016-11-18 10:47:33 -0700

    added migration file with CREATE tables and added ERD picture

---
+++ migration.sql
@@ -0,0 +1,33 @@
+CREATE TABLE publishers (
+  id serial PRIMARY KEY,
+  name text
+);
+
+CREATE TABLE series (
+  id serial PRIMARY KEY,
+  pub_id numeric REFERENCES publishers (id),
+  name text,
+  overview text,
+  genre text
+);
+
+CREATE TABLE issues (
+  id serial PRIMARY KEY,
+  series_id numeric REFERENCES series (id),
+  name text,
+  cover_img text,
+  plot text,
+  issue_num numeric
+);
+
+CREATE TABLE writers (
+  id serial PRIMARY KEY,
+  name text,
+  issue_id numeric REFERENCES issues (id)
+);
+
+CREATE TABLE artists (
+  id serial PRIMARY KEY,
+  name text,
+  issue_id numeric REFERENCES issues (id)
+);
