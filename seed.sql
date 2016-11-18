e796090f0c95b26577cf6854312124d10b336f83
Kyle Gray <th33nd3r@gmail.com>
2016-11-18 10:56:42 -0700

    started sudo-code for seed file

---
+++ seed.sql
@@ -0,0 +1,9 @@
+INSERT INTO publishers (name) VALUES ('');
+
+INSERT INTO series (name, overview, genre) VALUES ('', '', '');
+
+INSERT INTO issues (name, cover_img, plot, issue_num) VALUES ('', '', '', '');
+
+INSERT INTO writers (name) VALUES ('');
+
+INSERT INTO artists (name) VALUES ('');
